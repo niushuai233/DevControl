90e3d64f35aa677a9ec002b998a8aa3ab3816b09
niushuai233 <niushuai951101@gmail.com>
2022-10-17 17:25:31 +0800

    feat: 添加设备类型枚举类

---
+++ app/src/main/java/cc/niushuai/project/devcontrol/base/enums/DeviceTypeEnum.java
@@ -0,0 +1,21 @@
+package cc.niushuai.project.devcontrol.base.enums;
+
+/**
+ * 设备类型枚举
+ *
+ * @author niushuai
+ * @date: 2022/10/17 16:48
+ */
+public enum DeviceTypeEnum {
+
+    /**
+     * 开关
+     */
+    Switch,
+
+    /**
+     * 未知
+     */
+    Unknown;
+
+}
